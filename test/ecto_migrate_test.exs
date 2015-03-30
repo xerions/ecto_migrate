12c831e82ad28cdeb99e137df8e862cb927cb23d
0xAX <kuleshovmail@gmail.com>
2015-03-30 15:00:03 +0600

    integrated ecto_it into ecto_migration

--- test/ecto_migrate_test.exs
+++ test/ecto_migrate_test.exs
@@ -1,8 +1,21 @@
+defmodule TestModel do
+  use Ecto.Schema
+  schema "ecto_migrate test table" do
+    field :f, :string
+    field :i, :integer
+  end
+end
+
 defmodule EctoMigrateTest do
   use ExUnit.Case
-  doctest EctoMigrate
-
-  test "the truth" do
-    assert 1 + 1 == 2
+  import Ecto.Query
+  test "ecto_migrate test" do
+    :ok = :application.start(:ecto_it)
+    Ecto.Migration.Auto.migrate(EctoIt.Repo, TestModel)
+    query = from t in Ecto.Migration.SystemTable, select: t
+    [result] = EctoIt.Repo.all(query)
+    assert result.metainfo == "f:string,i:integer"
+    assert result.tablename == "ecto_migrate test table"
+    :ok = :application.stop(:ecto_it)
   end
 end
