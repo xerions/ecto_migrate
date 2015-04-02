abe8828ebfc3086d39a5741b1ae53fc0d184b2e4
0xAX <kuleshovmail@gmail.com>
2015-04-02 15:50:59 +0600

    Custom types support

--- test/ecto_migrate_test.exs
+++ test/ecto_migrate_test.exs
@@ -3,6 +3,7 @@ defmodule TestModel do
   schema "ecto_migrate test table" do
     field :f, :string
     field :i, :integer
+    field :j, Ecto.DateTime
   end
 end
 
@@ -14,7 +15,7 @@ defmodule EctoMigrateTest do
     Ecto.Migration.Auto.migrate(EctoIt.Repo, TestModel)
     query = from t in Ecto.Migration.SystemTable, select: t
     [result] = EctoIt.Repo.all(query)
-    assert result.metainfo == "f:string,i:integer"
+    assert result.metainfo == "f:string,i:integer,j:Elixir.Ecto.DateTime"
     assert result.tablename == "ecto_migrate test table"
     :ok = :application.stop(:ecto_it)
   end
