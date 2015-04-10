353b089011efd68e058c6c7e82fa13c94162cf91
0xAX <kuleshovmail@gmail.com>
2015-04-10 19:42:31 +0600

    [ecto_migrate] Support for indexes

--- lib/ecto/migration/system_table.ex
+++ lib/ecto/migration/system_table.ex
@@ -1,3 +1,30 @@
+defmodule Ecto.Migration.SystemTable.Index.Migration do
+  use Ecto.Migration
+  def up do
+    create table(:ecto_auto_migration_index) do
+      add :tablename, :string
+      add :index, :string
+      add :name, :string
+      add :concurrently
+      add :unique, :boolean
+      add :using, :string
+    end
+  end
+end
+
+defmodule Ecto.Migration.SystemTable.Index do
+  use Ecto.Model
+  @derive [Access]
+  @primary_key {:tablename, :string, []}
+  schema "ecto_auto_migration_index" do
+    field :index, :string
+    field :name, :string
+    field :concurrently, :boolean
+    field :unique, :boolean
+    field :using, :string
+  end
+end
+
 defmodule Ecto.Migration.SystemTable.Migration do
   use Ecto.Migration
   def up do
